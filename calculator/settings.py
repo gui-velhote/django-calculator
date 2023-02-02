c05ab21a67692d40325fada7bd017ac3fa75b2ed
gui-velhote <gui.velhote@gmail.com>
2023-02-01 21:08:53 -0300

    updated security

--- calculator/settings.py
+++ calculator/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*p0no(nsn(#(&bq^9-=8%(h1l(0j#vys*%=a#p%@4s(1ny6pvc'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'calcmain',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
